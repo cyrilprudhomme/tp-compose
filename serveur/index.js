540b4ecee10c99af3d8f58814709296fc66aaa1a
SIU\extcprudhomme <cyril.prudhomme.ext@cibtp.fr>
2023-03-10 08:54:09 +0100

    Fix TP PORT

--- serveur/index.js
+++ serveur/index.js
@@ -3,14 +3,15 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {cors: {origin: '*'}});
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
+        console.log('Message recu => '+ msg)
     });
 });
 
-server.listen(3000, () => {
+server.listen(4000, () => {
     console.log('listening on *:3000');
 });
